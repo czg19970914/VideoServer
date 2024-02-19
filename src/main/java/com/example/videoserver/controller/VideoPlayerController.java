50f1bf3d6a46a8f6456230cac5045455e3f4d8da
czg19970914 <chengzhigui97@163.com>
2024-02-19 22:33:33 +0800

    视频播放

--- src/main/java/com/example/videoserver/controller/VideoPlayerController.java
+++ src/main/java/com/example/videoserver/controller/VideoPlayerController.java
@@ -1,2 +1,30 @@
-package com.example.videoserver.controller;public class VideoPlayerController {
+package com.example.videoserver.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+public class VideoPlayerController {
+    @GetMapping(value = "/video")
+    public ResponseEntity<InputStreamResource> getVideo() throws IOException {
+//        ClassPathResource videoFile = new ClassPathResource("D:\\apache-tomcat-8.5.93\\webapps\\videos\\Bilibili\\洛丽塔大哥\\2.mp4");
+        File videoFile = new File("D:\\apache-tomcat-8.5.93\\webapps\\videos\\Bilibili\\洛丽塔大哥\\2.mp4");
+        InputStream inputStream = new FileInputStream(videoFile);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("video/mp4"));
+
+        return new ResponseEntity<>(new InputStreamResource(inputStream), headers, HttpStatus.OK);
+    }
 }
